00d335030c94f4eff17122ca258640b4041b3053
RevRebel <93675875+helpdesk-at-REVREBEL@users.noreply.github.com>
2023-06-01 02:05:01 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,41 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### [0.1.3](https://github.com/rogalmic/vscode-zsh-debug/compare/v0.1.2...v0.1.3) (2019-09-06)
+
+
+### Bug Fixes
+
+* Set proper default value for terminalKind. ([f7c9930](https://github.com/rogalmic/vscode-zsh-debug/commit/f7c9930))
+
+### [0.1.2](https://github.com/rogalmic/vscode-zsh-debug/compare/v0.1.1...v0.1.2) (2019-05-21)
+
+
+### Bug Fixes
+
+* Set debug console as default `terminalKind` ([2369712](https://github.com/rogalmic/vscode-zsh-debug/commit/2369712))
+
+
+### BREAKING CHANGES
+
+* interactive scripts need to select proper terminalKind
+
+
+
+### [0.1.1](https://github.com/rogalmic/vscode-zsh-debug/compare/v0.1.0...v0.1.1) (2019-02-27)
+
+
+### Features
+
+* Add `debugConsole` as new `terminalKind` ([2fa48e1](https://github.com/rogalmic/vscode-zsh-debug/commit/2fa48e1))
+* Pull recent changes from bash debug. ([e80a7dc](https://github.com/rogalmic/vscode-zsh-debug/commit/e80a7dc))
+
+
+
+## 0.1.0 (2018-10-03)
+
+
+### Features
+
+* First preview version based on `bashdb` and `vscode-bash-debug`
---
+++ src/extension.ts
@@ -0,0 +1,138 @@
+import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import { expandPath, getWSLPath } from './handlePath';
+import { normalize, join } from 'path';
+import * as which from 'npm-which';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.zsh-debug.getProgramName', _config => {
+		// Invoked if any property in client's launch.json has ${command:AskForScriptName} (mapped to getProgramName
+		// in package.json) in its value.
+		return vscode.window.showInputBox({
+			placeHolder: "Type absolute path to zsh script.",
+			value: (process.platform === "win32") ? "{workspaceFolder}\\path\\to\\script.sh" : "{workspaceFolder}/path/to/script.sh"
+		}).then(v => expandPath(v, vscode.workspace.rootPath));
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.zsh-debug.selectProgramName', _config => {
+		// Invoked if any property in client's launch.json has ${command:SelectScriptName} (mapped to selectProgramName
+		// in package.json) in its value.
+		return vscode.workspace.findFiles("**/*.sh", "").then((uris) => {
+			const list = new Array<string>();
+			for (let i = 0; i < uris.length; i++) {
+				list.push(uris[i].fsPath);
+			}
+			return vscode.window.showQuickPick(list).then((result) => {
+				if (!result) {
+					return undefined; // canceled, abort launch
+				}
+				return result;
+			}).then(v => expandPath(v, vscode.workspace.rootPath));
+		});
+	}));
+
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('zshdb', new ZshConfigurationProvider()));
+}
+
+export function deactivate() {
+	// nothing to do
+}
+
+class ZshConfigurationProvider implements vscode.DebugConfigurationProvider {
+	/**
+	 * Check configuration just before a debug session is being launched.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, _token?: CancellationToken): ProviderResult<DebugConfiguration> {
+		if (!config.type && !config.request && !config.name) {
+			// If launch.json is missing or empty, abort launch and create launch.json with "initialConfigurations"
+			return undefined;
+		}
+
+		if (!folder) {
+			let msg = "Unable to determine workspace folder.";
+			return vscode.window.showErrorMessage(msg).then(_ => { return undefined; });
+		}
+
+		// Else launch.json exists
+		if (!config.type || !config.name) {
+			let msg = "BUG in Zsh Debug: reached to unreachable code.";
+			msg += "\nIf it is reproducible, please report this bug on: https://github.com/rogalmic/vscode-zsh-debug/issues";
+			msg += "\nYou can avoid this bug by setting \"type\" and \"name\" attributes in launch.json.";
+			return vscode.window.showErrorMessage(msg).then(_ => { return undefined; });
+		}
+		if (!config.request) {
+			let msg = "Please set \"request\" attribute to \"launch\".";
+			return vscode.window.showErrorMessage(msg).then(_ => { return undefined; });
+		}
+
+		// Abort launch if any deprecated argument is included
+		if (config.zshDbPath) {
+			return vscode.window.showErrorMessage("`zshDbPath` is deprecated. Use `pathZshdb` instead.").then(_ => { return undefined; });
+		}
+		if (config.zshPath) {
+			return vscode.window.showErrorMessage("`zshPath` is deprecated. Use `pathZsh` instead.").then(_ => { return undefined; });
+		}
+		if (config.commandLineArguments) {
+			return vscode.window.showErrorMessage("`commandLineArguments` is deprecated. Use `args` instead.").then(_ => { return undefined; });
+		}
+		if (config.scriptPath) {
+			return vscode.window.showErrorMessage("`scriptPath` is deprecated. Use `program` instead.").then(_ => { return undefined; });
+		}
+
+		// Check "required" attributes (defined in package.json) are included
+		if (!config.program) {
+			return vscode.window.showErrorMessage("Please specify \"program\" in launch.json.").then(_ => { return undefined; });
+		}
+
+		// Fill non-"required" attributes with default values to prevent zshdb (or other programs) from panic
+		if (!config.args) { config.args = []; }
+		if (!config.env) { config.env = {}; }
+		if (!config.cwd) { config.cwd = folder.uri.fsPath; }
+		if (!config.pathZsh) {
+			config.pathZsh = "zsh";
+		}
+		if (!config.pathZshdb) {
+			if (process.platform === "win32") {
+				config.pathZshdb = getWSLPath(normalize(join(__dirname, "..", "zshdb_dir", "zshdb")));
+			}
+			else {
+				config.pathZshdb = normalize(join(__dirname, "..", "zshdb_dir", "zshdb"));
+			}
+		}
+		if (!config.pathZshdbLib) {
+			if (process.platform === "win32") {
+				config.pathZshdbLib = getWSLPath(normalize(join(__dirname, "..", "zshdb_dir")));
+			}
+			else {
+				config.pathZshdbLib = normalize(join(__dirname, "..", "zshdb_dir"));
+			}
+		}
+
+		if (!config.pathCat) { config.pathCat = "cat"; }
+		if (!config.pathMkfifo) { config.pathMkfifo = "mkfifo"; }
+		if (!config.pathPkill) {
+
+			if ( process.platform === "darwin" ) {
+				const pathPkill = which(__dirname).sync('pkill');
+				if (pathPkill === "/usr/local/bin/pkill") {
+					vscode.window.showInformationMessage(`Using /usr/bin/pkill instead of /usr/local/bin/pkill`);
+					config.pathPkill = "/usr/bin/pkill";
+				}
+				else {
+					config.pathPkill = "pkill";
+				}
+			}
+			else {
+				config.pathPkill = "pkill";
+			}
+		}
+
+		// These variables can be undefined, as indicated in `?` (optional type) in zshDebug.ts:LaunchRequestArguments
+		// - config.showDebugOutput
+		// - config.trace
+		// - config.terminalKind
+
+		return config;
+	}
+}
